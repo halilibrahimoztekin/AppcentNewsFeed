b505b58e1498317d767fb430c8744010d1561e2a
Halil Ibrahim Oztekin <35626045+halilibrahimoztekin@users.noreply.github.com>
2021-10-26 01:07:06 +0300

    Initial Commit

---
+++ AppcentNewsFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppcentNewsFeed
+//
+//  Created by Bukefalos on 26.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
