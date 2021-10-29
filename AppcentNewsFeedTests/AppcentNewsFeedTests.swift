0e3cd8970ed69a221cd83e65f613e96becb398be
Halil Ibrahim Oztekin <35626045+halilibrahimoztekin@users.noreply.github.com>
2021-10-29 15:54:40 +0300

    editing readme

--- AppcentNewsFeedTests/AppcentNewsFeedTests.swift
+++ AppcentNewsFeedTests/AppcentNewsFeedTests.swift
@@ -9,8 +9,13 @@ import XCTest
 @testable import AppcentNewsFeed
 
 class AppcentNewsFeedTests: XCTestCase {
+    
+    var keyword = ""
+    var pagination = 1000
+    var apiKey = ""
 
     override func setUpWithError() throws {
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
@@ -19,6 +24,8 @@ class AppcentNewsFeedTests: XCTestCase {
     }
 
     func testExample() throws {
+        
+        webService().getFeed(apiKey: apiKey, keyword: keyword, pagination: pagination, completion: Article)
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
